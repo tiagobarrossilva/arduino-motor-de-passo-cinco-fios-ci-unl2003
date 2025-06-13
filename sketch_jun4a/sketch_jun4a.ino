40c658021349a5dd59f34373a5b4c02912b8708e
Tiago Barros <tiago.barrossilva@outlook.com>
2025-06-13 11:39:42 -0300

    update

--- sketch_jun4a/sketch_jun4a.ino
+++ sketch_jun4a/sketch_jun4a.ino
@@ -1,7 +1,7 @@
 // Desenvolvido por Tiago Barros em 04/06/2025
 // Este codigo foi testado em um Arduino Nano com um motor de passo de cinco fios modelo 24BYJ, usando o CI uln2003
 // O número de passos por volta (para o 24BYJ) geralmente é 512 passos
-// O motor de passo foi testado em velocidades de 2 a 10 ms
+// O motor de passo foi testado em velocidades de 3 a 10 ms
 
 // Define os pinos de controle do motor de passo
 const byte pin1 = 8; // Pino IN1 do ci
@@ -14,26 +14,18 @@ void girar(int passos, bool sentidoHorario, byte velocidade) {
   // sentido horário
   if(sentidoHorario == true){
     for (int i = 0; i < passos; i++) {
-      digitalWrite(pin1, HIGH);
-      digitalWrite(pin2, LOW);
-      digitalWrite(pin3, LOW);
       digitalWrite(pin4, LOW);
+      digitalWrite(pin1, HIGH);
       delay(velocidade);
 
       digitalWrite(pin1, LOW);
       digitalWrite(pin2, HIGH);
-      digitalWrite(pin3, LOW);
-      digitalWrite(pin4, LOW);
       delay(velocidade);
 
-      digitalWrite(pin1, LOW);
       digitalWrite(pin2, LOW);
       digitalWrite(pin3, HIGH);
-      digitalWrite(pin4, LOW);
       delay(velocidade);
 
-      digitalWrite(pin1, LOW);
-      digitalWrite(pin2, LOW);
       digitalWrite(pin3, LOW);
       digitalWrite(pin4, HIGH);
       delay(velocidade);
@@ -42,30 +34,26 @@ void girar(int passos, bool sentidoHorario, byte velocidade) {
   } else{
     for (int i = 0; i < passos; i++) {
       digitalWrite(pin1, LOW);
-      digitalWrite(pin2, LOW);
-      digitalWrite(pin3, LOW);
       digitalWrite(pin4, HIGH);
       delay(velocidade);
 
-      digitalWrite(pin1, LOW);
-      digitalWrite(pin2, LOW);
+      digitalWrite(pin4, LOW);
       digitalWrite(pin3, HIGH);
-      digitalWrite(pin4, LOW);
       delay(velocidade);
 
-      digitalWrite(pin1, LOW);
+      digitalWrite(pin3, LOW);
       digitalWrite(pin2, HIGH);
-      digitalWrite(pin3, LOW);
-      digitalWrite(pin4, LOW);
       delay(velocidade);
 
-      digitalWrite(pin1, HIGH);
       digitalWrite(pin2, LOW);
-      digitalWrite(pin3, LOW);
-      digitalWrite(pin4, LOW);
+      digitalWrite(pin1, HIGH);
       delay(velocidade);
     }
-  }  
+  }
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin2, LOW);
+  digitalWrite(pin3, LOW);
+  digitalWrite(pin4, LOW);
 }
 
 void setup() {
@@ -77,10 +65,10 @@ void setup() {
 
 void loop() {
   // Gira o motor uma volta no sentido horário
-  girar(512,true,2);
+  girar(512,true,5);
   delay(3000);
   
-  // Gira o motor uma volta no sentido anti-horário mais lentamente
+  // Gira o motor uma volta no sentido anti-horário
   girar(512,false,5);
   delay(3000);
 }
